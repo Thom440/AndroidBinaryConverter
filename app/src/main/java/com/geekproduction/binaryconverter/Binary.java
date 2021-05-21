2eeb2ce3e0bc625454963616f168784ac0af4858
Thom Barrett <thom440@yahoo.com>
2021-05-21 12:22:07 -0700

    The binary activity is started

--- app/src/main/java/com/geekproduction/binaryconverter/Binary.java
+++ app/src/main/java/com/geekproduction/binaryconverter/Binary.java
@@ -5,11 +5,22 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.math.BigInteger;
 
 public class Binary extends AppCompatActivity {
+    private TextView decimalText;
+    private TextView hexText;
+    private TextView octalText;
+    private TextView binaryText;
+    private TextView complementText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +30,63 @@ public class Binary extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar3);
         setSupportActionBar(myToolbar);
         myToolbar.setOverflowIcon(ContextCompat.getDrawable(this, R.drawable.menu_icon));
-        getSupportActionBar().setTitle("Binary");
+        getSupportActionBar().setTitle("Binary Conversion");
+
+        decimalText = (TextView)findViewById(R.id.decimalTextView2);
+        decimalText.setText("");
+
+        binaryText = (TextView)findViewById(R.id.binaryTextView2);
+        binaryText.setText("");
+
+        octalText = (TextView)findViewById(R.id.octalTextView2);
+        octalText.setText("");
+
+        hexText = (TextView)findViewById(R.id.hexTextView2);
+        hexText.setText("");
+
+        complementText = (TextView)findViewById(R.id.complimentView);
+        complementText.setText("");
+    }
+
+    public void onClick(View v) {
+        String binaryTextValue = (String) binaryText.getText();
+        if (((Button)v).getText().equals("")) {
+            if (!(binaryText.getText().equals(""))) {
+                binaryTextValue = binaryTextValue.substring(0, binaryTextValue.length() - 1);
+            }
+        }
+        else {
+            binaryTextValue += (String)((Button)v).getText();
+        }
+        binaryText.setText(binaryTextValue);
+        if (Validator.validBigInteger(binaryTextValue)) {
+            BigInteger bigInt = new BigInteger(String.valueOf(binaryTextValue));
+            new DoConversions().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, bigInt);
+        }
+        else {
+            decimalText.setText("");
+            octalText.setText("");
+            hexText.setText("");
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("MyBoolean", true);
+        outState.putString("Binary", (String)binaryText.getText());
+        outState.putString("Decimal", (String)decimalText.getText());
+        outState.putString("Octal", (String)octalText.getText());
+        outState.putString("Hex", (String)hexText.getText());
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        binaryText.setText(savedInstanceState.getString("Binary"));
+        decimalText.setText(savedInstanceState.getString("Decimal"));
+        octalText.setText(savedInstanceState.getString("Octal"));
+        hexText.setText(savedInstanceState.getString("Hex"));
     }
 
     @Override
@@ -50,4 +117,38 @@ public class Binary extends AppCompatActivity {
         startActivity(intent);
         return super.onOptionsItemSelected(item);
     }
+
+    private class DoConversions extends AsyncTask<BigInteger, Void, String[]> {
+        @Override
+        protected String[] doInBackground(BigInteger... bigInt) {
+            String decimal = Convert.binaryToDecimal(bigInt[0]);
+            String twosComplement = Convert.findTwosComplement(decimal);
+            BigInteger decimalBigInt = new BigInteger(decimal);
+            String octal = Convert.decimalToOctal(decimalBigInt);
+            String hex = Convert.decimalToHex(decimalBigInt);
+
+            String[] result = {octal, hex, decimal, twosComplement};
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String... result) {
+            octalText.setText(result[0]);
+            hexText.setText(result[1]);
+            decimalText.setText(result[2]);
+            complementText.setText(result[3]);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate();
+        }
+    }
+
+
 }
--- app/src/main/java/com/geekproduction/binaryconverter/Convert.java
+++ app/src/main/java/com/geekproduction/binaryconverter/Convert.java
@@ -54,4 +54,40 @@ public class Convert {
         while (bigInt.compareTo(BigInteger.ZERO) > 0);
         return hex;
     }
+
+    public static String binaryToDecimal(BigInteger bigInt) {
+        int i = 0;
+        BigInteger constantTwo = new BigInteger("2");
+        BigInteger finalNumber = BigInteger.ZERO;
+
+        while (bigInt.compareTo(BigInteger.ZERO) > 0) {
+            BigInteger decimalNumber = bigInt.mod(BigInteger.valueOf(10)).multiply(constantTwo.pow(i));
+            finalNumber = finalNumber.add(decimalNumber);
+            bigInt = bigInt.divide(BigInteger.valueOf(10));
+            i++;
+        }
+        return finalNumber.toString();
+    }
+
+    public static String findTwosComplement(String decimalText) {
+        if (Validator.validBigInteger(decimalText)) {
+            BigInteger bigInt = new BigInteger(decimalText);
+            BigInteger intMaxTwosComplement = new BigInteger("4294967295");
+            BigInteger intMinTwosComplement = new BigInteger("2147483648");
+            BigInteger longTwosComplement = new BigInteger("18446744073709551615");
+            if (bigInt.compareTo(BigInteger.valueOf(65535)) <= 0 && bigInt.compareTo(BigInteger.valueOf(32768)) >= 0) {
+                BigInteger newDecimal = bigInt.add(BigInteger.valueOf(Short.MIN_VALUE))
+                        .subtract(BigInteger.valueOf(Short.MAX_VALUE))
+                        .subtract(BigInteger.ONE);
+                return newDecimal.toString();
+            }
+            else if (bigInt.compareTo(intMaxTwosComplement) <= 0 && bigInt.compareTo(intMinTwosComplement) >= 0) {
+                BigInteger newDecimal = bigInt.add(BigInteger.valueOf(Integer.MIN_VALUE))
+                        .subtract(BigInteger.valueOf(Integer.MAX_VALUE))
+                        .subtract(BigInteger.ONE);
+                return newDecimal.toString();
+            }
+        }
+        return "N/A";
+    }
 }
